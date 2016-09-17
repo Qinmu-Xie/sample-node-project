e949cffb2d4b9be059992adc247305986a81124f
qmxie <qmxie@thoughtworks.com>
2016-09-17 11:06:28 +0800

    [#0] add continuors deploment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-docker build -t qinmux/sample-node .
 docker push qinmux/sample-node
 
+ssh ubuntu@54.222.179.18 << EOF
 docker pull qinmux/sample-node:latest
 docker stop web || true
 docker rm web || true
 docker rmi qinmux/sample-node:current || true
 docker tag qinmux/sample-node:latest qinmux/sample-node:current
 docker run -d --restart always --name web -p 3000:3000 qinmux/sample-node:current
+EOF
