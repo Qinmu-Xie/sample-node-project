7ed848c33c26c47cc3810913ae98e1486304e720
qmxie <qmxie@thoughtworks.com>
2016-09-17 11:17:42 +0800

    [#0] daocloud need sudo to run docker cmd

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 docker push qinmux/sample-node
 
 ssh ubuntu@54.222.179.18 << EOF
-docker pull qinmux/sample-node:latest
-docker stop web || true
-docker rm web || true
-docker rmi qinmux/sample-node:current || true
-docker tag qinmux/sample-node:latest qinmux/sample-node:current
-docker run -d --restart always --name web -p 3000:3000 qinmux/sample-node:current
+sudo docker pull qinmux/sample-node:latest
+sudo docker stop web || true
+sudo docker rm web || true
+sudo docker rmi qinmux/sample-node:current || true
+sudo docker tag qinmux/sample-node:latest qinmux/sample-node:current
+sudo docker run -d --restart always --name web -p 3000:3000 qinmux/sample-node:current
 EOF
